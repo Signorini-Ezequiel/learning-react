7c91976278eef4339dae2c0b92a5944e681b282c
Ezequiel <ezequiel.signorini@gmail.com>
2022-10-25 20:00:17 -0300

    agrego la base para el uso de useState

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -14,6 +14,9 @@ function Home() {
         navigate(url);
     };
 
+    // Establezco un hook, use state para cambiar el color de un botón cuando se le haga click
+    const [color, setColor] = useState(initialColor);
+
     // Establesco el código a retornar
     return (
         <div className="App">
@@ -23,10 +26,12 @@ function Home() {
                 {/* Uso el componente, poniendo la información de sus props */}
                 {/* <Button content="Saber más" onclick={()=>alert(alert="La funcionalidad no está creada, saludos")}/> */}
                 {/* Creo un botón que implementa la función onClickButton para ir a la ruta indicada */}
-                <Button onclick={()=>onClickButton("/game")} content="Ezequiel Signorini"/>
+                <Button onclick={() => onClickButton("/game")} content="Ezequiel Signorini"/>
 
                 {/* Para ir a un sitio puedo usar Link to, que funciona como un a href */}
                 {/* <Link to="/">Inicio</Link> */}
+                {/* Creo un botón que cambie de color con el uso de useState */}
+                
             </header>
         </div>
     );
