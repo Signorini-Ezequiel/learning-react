bfce1cfea0f86b7ed787839ebaefb3a1c67a7aea
Ezequiel <ezequiel.signorini@gmail.com>
2022-11-24 08:53:01 -0300

    add info for the site

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import './App.css';
 import 'bulma/css/bulma.min.css';
 // Importo React-router-dom que es una librería de react para manejar rutas
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// Importo json para agregar la ruta base del sitio
+import packageInfo from '../package.json';
 // Importo las páginas a rutear
 import Home from './pages/home';
 import Game from './pages/game';
 
 function App() {
+  function getBasename() {
+    let array = packageInfo.homepage.split('/');
+    return array[array.length-1];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         {/* BrowserRouter nos permite conectar nuestra aplicación con la URL del browser */}
-        <BrowserRouter>
+        <BrowserRouter basename={getBasename()}>
         {/* Routes y route son los elementos que nos permiten configurar las rutas de nuestra aplicación */}
           <Routes>
             {/* Las rutas deben tener un path y un element */}
